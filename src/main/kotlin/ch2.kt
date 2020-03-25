58796c0802f6763cd402fc72151c79948a917c77
Yuchi Tung <tracy770830@gmail.com>
2020-03-25 19:20:09 +0800

    feat: add ch2 challenges

---
+++ src/main/kotlin/ch2.kt
@@ -0,0 +1,17 @@
+fun main() {
+    val playerName = "Link"
+    var hasSteed: Boolean = false
+    var coins: Int = 50
+
+    val bistroName: String = "Telma's Bar"
+    val bistroOwner: String = "Telma"
+    val drinks: Map<String, Int> = mapOf(
+        "Martini" to 40,
+        "Gimlet" to 35,
+        "MoscowMule" to 35
+    )
+
+    println("Player name:" + playerName)
+    println("Player name:" + bistroName)
+    println("Estragon".reversed())
+}
